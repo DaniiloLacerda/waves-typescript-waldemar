95e9daac9e1ad3bc18f83b0829c6a2ae9f90df43
Danilo Lacerda <danilo.lacerda@totvs.com.br>
2021-01-16 20:50:58 -0300

    test echo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,10 @@
 
 # Init .ssh dir and expand $SSH_KEY
 mkdir -p ~/.ssh
-echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
+echo -e "${WAVES_KEY//_/\\n}" > ~/.ssh/id_rsa
 echo "ssh key"
-echo $SSH_KEY
-echo "A01"
-echo $A01
-echo "A02"
-echo $A02
+echo $WAVES_KEY
+cat ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # Add umbler's git to the list of known_hosts to prevent terminal for asking to confirm the host
